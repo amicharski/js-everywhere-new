15c76f0c16ba39c504d71ee2bfffafbba98fea74
unknown <amicharski@outlook.com>
2023-04-03 11:47:20 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 // index.js
 // This is the main entry point of our application
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { json } = require('body-parser');
+const cors = require('cors');
+
+const port = process.env.PORT || 4000;
+
+const typeDefs = `#graphql
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!'
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.start().then(() => {
+    const app = express();
+
+    app.use('/api', cors(), json(), expressMiddleware(server))
+    
+    app.listen({ port }, () => {
+        console.log(`http://localhost:${port}${server.graphqlPath}`)
+    });
+});
